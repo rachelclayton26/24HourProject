c3c033cf00afcbc846a590b672d38bab40cb90f9
rachelclayton26 <75646291+rachelclayton26@users.noreply.github.com>
2021-04-21 07:16:14 -0500

    Delete backgroundImg.js

--- client/src/components/backgroundImg.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import Geolocate from "./geolocation";
-
-const key = QWSG9LDFHEX4U9EY3INLKR4F8KR8XUI5;
-const imgUrl=`https://api.geodatasource.com/city?key=${key}&format=json&${lat}&${lng}`
