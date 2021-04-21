9bed23b06468d0743f7c28e37087323eaf63be97
Rachel Christopher <rachelchristopher@Rachels-MacBook-Pro.local>
2021-04-21 12:17:03 -0500

    final commit

--- client/src/components/Geolocation.js
+++ client/src/components/Geolocation.js
@@ -1,15 +1,15 @@
-const Geolocate = (props) => {
-  const latLng = (lat, lng) => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(getPosition);
-    }
-    function getPosition(position) {
-      lat = position.coords.latitude;
-      lng = position.coords.longitude;
-      console.log(lat, lng);
-    }
-  };
-  let getLatLng = latLng();
-  return getLatLng;
-};
-export default Geolocate;
+// const Geolocate = (props) => {
+//   const latLng = (lat, lng) => {
+//     if (navigator.geolocation) {
+//       navigator.geolocation.getCurrentPosition(getPosition);
+//     }
+//     function getPosition(position) {
+//       lat = position.coords.latitude;
+//       lng = position.coords.longitude;
+//       console.log(lat, lng);
+//     }
+//   };
+//   let getLatLng = latLng();
+//   return getLatLng;
+// };
+// export default Geolocate;
--- client/src/components/NASA.js
+++ client/src/components/NASA.js
@@ -1,32 +1,47 @@
 import { useState, useEffect } from "react";
+// import Geolocate from './Geolocation';
+
 const key = "OvrxrgwbLdNgnfNdzJ2XBn7FjNPeo3qXF4i0byF78";
 const baseUrl = "https://api.nasa.gov/planetary/earth/imagery";
 
-const Nasa = (props) => {
-  const latitude = props.coords.lat;
-  const longitude = props.coords.lon;
-  const dates = new Date().toISOString().split("T")[0];
+let urlForImage;
 
-  const urlForImage = `${baseUrl}?lon=${longitude}&lat=${latitude}&date=${dates}&dim=0.19&api_key=${key}`;
+const Nasa = () => {
+  const [geolocation, setGeolocation] = useState();
+  const [result, setResult] = useState();
 
-  const [result, setResult] = useState({});
+  if('geolocation' in navigator) {
+    console.log("geolocation is available");
+    console.log(navigator.geolocation.getCurrentPosition)
+  } else {
+    console.log("geolocation IS NOT available");
+  }
 
-  useEffect(() => {
-    fetchImage();
-  });
+  if (geolocation) {
+    const latitude = geolocation.coords.lat;
+    const longitude = geolocation.coords.lon; 
+    const dates = new Date().toISOString().split("T")[0];
+    urlForImage = `${baseUrl}?lon=${longitude}&lat=${latitude}&date=${dates}&dim=0.19&api_key=${key}`;
+    // urlForImage= 'https://api.nasa.gov/planetary/earth/imagery?lon=-86.148003&lat=39.791000&date=2014-02-01&api_key=na5CzAGj6sT9mL17haLlVCOJ646wZCYXYb8OyKs2';
+  }
+    useEffect(() => {
+      if(urlForImage) {
+        fetchImage();
+      }
+    },[]);
 
   const fetchImage = async () => {
     console.log(urlForImage);
     fetch(urlForImage)
       .then((res) => {
-        console.log("res:", res);
-        return res.url;
+        console.log("success:", res);
+        return setResult(res.url);
       })
-      .then((res) => {
-        console.log(res);
-        setResult(res);
+      .catch(error => {
+        console.error('Error:', error);
       });
-  };
+
+    };
   return (
     <div>
       <img src={result} alt="pic"></img>
