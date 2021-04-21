de4600995f909d5d94ae9f8d394515a030e0d378
Rachel Christopher <rachelchristopher@Rachels-MacBook-Pro.local>
2021-04-20 22:52:25 -0500

    html/css

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+<link href="http://fonts.cdnfonts.com/css/brusher" rel="stylesheet"></link>
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav">
+        <div className="title">Local City Fact Finder</div>
+        <form className="search">
+          <button className="searchButton">Search</button>
+          <input className="searchImput" type="text" placeholder="City, State"/>
+        </form>
+      </div>
+      <div className="imageCropper">
+        <img id="fetchImg" src=""></img>
+      </div>
+      <div className="center">
+        <div class="city-state">
+          <div className="city">Indianapolis</div>
+          <div className="state">Indiana</div>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col-sm">
+              <div className="header">Bird's Eye View</div>
+              <div className="fetchContainers">
+                <div className="nasaFetch"></div>
+              </div>
+            </div>
+            <div class="col-sm">
+            <div className="header">Live Weather</div>
+              <div className="fetchContainers">
+                <div className="weatherFetch"></div>
+              </div>
+            </div>
+            <div class="col-sm">
+            <div className="header">Local Eats</div>
+              <div className="fetchContainers">
+                <div className="eatsFetch"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+      <div className="footer">
+        <div className="copyright">The Fantastic Four   |   Copyright 2021</div>
+      </div>
     </div>
   );
 }
